8b0e86a58b40708716d56ebbde35e6ed8e1fdbab
acv593 <luca.gebhardt@haw-hamburg.de>
2023-03-26 19:59:58 +0200

    Implemented first version of HCA

--- src/main/java/de/heaal/eaf/algorithm/HillClimbingAlgorithm.java
+++ src/main/java/de/heaal/eaf/algorithm/HillClimbingAlgorithm.java
@@ -2,17 +2,17 @@
  * Evolutionary Algorithms Framework
  *
  * Copyright (c) 2023 Christian Lins <christian.lins@haw-hamburg.de>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,30 +31,48 @@ import de.heaal.eaf.base.Individual;
 import de.heaal.eaf.base.IndividualFactory;
 import de.heaal.eaf.mutation.Mutation;
 import de.heaal.eaf.mutation.MutationOptions;
+
 import java.util.Comparator;
 
 /**
  * Implementation of the Hill Climbing algorithm.
- * 
+ *
  * @author Christian Lins <christian.lins@haw-hamburg.de>
  */
 public class HillClimbingAlgorithm extends Algorithm {
 
     private final IndividualFactory indFac;
     private final ComparatorIndividual terminationCriterion;
-    
-    public HillClimbingAlgorithm(float[] min, float[] max, 
-            Comparator<Individual> comparator, Mutation mutator, 
-            ComparatorIndividual terminationCriterion) 
-    {
+
+    private final MutationOptions mutationOptions;
+
+    public HillClimbingAlgorithm(float[] min, float[] max,
+                                 Comparator<Individual> comparator, Mutation mutator,
+                                 ComparatorIndividual terminationCriterion) {
         super(comparator, mutator);
         this.indFac = new GenericIndividualFactory(min, max);
         this.terminationCriterion = terminationCriterion;
+        mutationOptions = new MutationOptions();
+        mutationOptions.put(MutationOptions.KEYS.MUTATION_PROBABILITY, 0.5f);
     }
-    
+
+    private boolean mutatedIndividualIsBetter(int compareResult) {
+        return compareResult > 0;
+    }
+
     @Override
     public void nextGeneration() {
         super.nextGeneration();
+        final int singleIndividualIndex = 0;
+        Individual chosenIndividual = population.get(singleIndividualIndex);
+        Individual copyOfChosenIndividual = chosenIndividual.copy();
+        mutator.mutate(copyOfChosenIndividual, mutationOptions);
+        int compareResult = comparator.compare(copyOfChosenIndividual, chosenIndividual);
+        if (mutatedIndividualIsBetter(compareResult)) {
+            population.set(singleIndividualIndex, copyOfChosenIndividual);
+            System.out.println(population.get(singleIndividualIndex).getGenome().toString());
+        }
+    }
 
         // HIER KÖNNTE DER ALGORITHMUS-LOOP STEHEN
     }
@@ -69,11 +87,9 @@ public class HillClimbingAlgorithm extends Algorithm {
     @Override
     public void run() {
         initialize(indFac, 1);
-        
-        while(!isTerminationCondition()) {
-            System.out.println("Next gen");
+        while (!isTerminationCondition()) {
             nextGeneration();
         }
-    }   
+    }
 
 }
